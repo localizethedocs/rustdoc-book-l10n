4a96eda80cad136e5b093c0acda41cd091744f0b
ltdrobot <ltdrobot@localizethedocs.org>
2026-09-15 13:59:34 +0000

    Deploying to pages from @ localizethedocs/rustdoc-book-l10n@80d6bcc094db6460404487fc3f9b734f5388a17b 🚀

---
+++ ltd-config.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["nightly", "Nightly"],
+    ["beta",    "Beta"],
+    ["stable",  "Stable"],
+    ["1",       "Release 1.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Home",    "https://localizethedocs.org/projects/rustdoc-book-l10n"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/rustdoc-book-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/rustdoc-book-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/rustdoc-book-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/rustdoc-book-l10n"],
+  ]
+};
